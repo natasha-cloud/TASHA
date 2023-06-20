4ba7f2a39f087d1cbd39b9ac81edcff236d6edc6
natasha-cloud <tindynatasha@gmail.com>
2023-06-20 16:05:07 +0300

    its js

---
+++ HELLO-WORLD.js
@@ -0,0 +1,59 @@
+console.log("Hello-world")
+
+//1.comments
+// used to provide information about the code
+// can be used in general to provide informations or suggestions
+// makes it easy to undertand the code or get more information aboutthe code
+// can also be used to comment out code sections
+// comments are ignored by javascript engine
+// comments can be single line or multiline
+// examples
+// this is a single line comment
+/*
+this is a multiline comment
+*/
+
+// 2.variables
+// used to stor4e data
+// can be looked at as container for storing variable containers
+// must contain only letters,digits and $ and _symbol
+
+// guidelines
+// must begin witha a letter or _
+// should not start with a number
+// must not be a reserved key word like if, else, booleanlet,var etc
+// varibles are case sensitive
+// use meaningful names
+// use camelcasing
+
+// var was used in a earliier but now we use let and const
+
+// examples
+// variable declaration
+let age;
+// variable initialization
+age = 23;
+//  = this is called an assignment opperator
+let studentName = "Nyamate";
+// cosole.log(studentName)
+// decalaring many variables on the same line
+let firstName, lastName, cohort, year;
+firstName = "Natasha";
+lastName = "Anita";
+cohort = 10;
+year = 2023;
+console.log (firstName)
+
+// dealing with contants
+const carName = "Subaru"   
+console.log(carName)
+carName = "Audi"
+console.log(carName)
+
+
+
+
+
+
+
+
